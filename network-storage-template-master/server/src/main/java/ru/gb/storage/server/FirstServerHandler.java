82feab49440258034944bc1b1f6a07ca53cd2148
Fokusmod <Info@mail.ru>
2022-04-22 20:59:18 +0300

    change server

--- network-storage-template-master/server/src/main/java/ru/gb/storage/server/FirstServerHandler.java
+++ network-storage-template-master/server/src/main/java/ru/gb/storage/server/FirstServerHandler.java
@@ -1,5 +1,7 @@
 package ru.gb.storage.server;
 
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import ru.gb.storage.common.message.*;
@@ -10,6 +12,8 @@ import java.util.Arrays;
 
 public class FirstServerHandler extends SimpleChannelInboundHandler<Message> {
 
+    private RandomAccessFile randomAccessFile = null;
+
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
         System.out.println("New active channel");
@@ -19,7 +23,7 @@ public class FirstServerHandler extends SimpleChannelInboundHandler<Message> {
     }
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, Message msg) {
+    protected void channelRead0(ChannelHandlerContext ctx, Message msg) throws IOException {
         if (msg instanceof TextMessage) {
             TextMessage message = (TextMessage) msg;
             System.out.println("incoming text message: " + message.getText());
@@ -38,26 +42,42 @@ public class FirstServerHandler extends SimpleChannelInboundHandler<Message> {
         if (msg instanceof FileRequestMessage) {
             FileRequestMessage frm = (FileRequestMessage) msg;
             final File file = new File(frm.getPath());
-            try (final RandomAccessFile raf = new RandomAccessFile(file, "r")) {
-                while (raf.getFilePointer() != raf.length()) {
-                    final byte[] fileContent;
-                    final long available = raf.length() - raf.getFilePointer();
-                    if (available > 100 * 1024) {
-                        fileContent = new byte[100 * 1024];
-                    } else {
-                        fileContent = new byte[(int) available];
+            randomAccessFile = new RandomAccessFile(file, "r");
+            sendFile(ctx);
+        }
+    }
+
+    private void sendFile(ChannelHandlerContext ctx) throws IOException {
+        if (randomAccessFile != null) {
+            final byte[] fileContent;
+            final long available = randomAccessFile.length() - randomAccessFile.getFilePointer();
+            if (available > 100 * 1024) {
+                fileContent = new byte[100 * 1024];
+            } else {
+                fileContent = new byte[(int) available];
+            }
+            final FileContentMessage fileContentMessage = new FileContentMessage();
+            fileContentMessage.setStartPosition(randomAccessFile.getFilePointer());
+            randomAccessFile.read(fileContent);
+            fileContentMessage.setContent(fileContent);
+            final boolean last = randomAccessFile.getFilePointer() == randomAccessFile.length();
+
+            fileContentMessage.setLastPosition(last);
+            ctx.writeAndFlush(fileContentMessage).addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                    if (!last) {
+                        sendFile(ctx);
                     }
-                    final FileContentMessage fileContentMessage = new FileContentMessage();
-                    fileContentMessage.setStartPosition(raf.getFilePointer());
-                    raf.read(fileContent);
-                    fileContentMessage.setContent(fileContent);
-                    fileContentMessage.setLastPosition(raf.getFilePointer() == raf.length());
-                    ctx.writeAndFlush(fileContentMessage);
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+            });
+            if (last) {
+                randomAccessFile.close();
+                randomAccessFile = null;
             }
         }
+
+
     }
 
     @Override
